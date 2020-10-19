169c0ff797bc4577d90b876cdd9a04e59f32223a
Pascal Seitz <pascal.seitz@sap.com>
2020-10-19 14:04:35 +0200

    inital

---
+++ src/bin/assert_contains_not.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::io::{self, Read};
+use std::process;
+
+fn main() {
+    let args: String = env::args().skip(1).collect::<Vec<String>>().join(" ");
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    if buffer.contains(&args) {
+        eprintln!("{:?} found", args);
+        process::exit(0x0100);
+    }
+}
